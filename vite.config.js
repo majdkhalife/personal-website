7d025999b6b0757f85be69e13b9404f0f5df7503
majdkhalife <majd.skhalife@gmail.com>
2025-07-29 22:01:23 -0400

    fddd

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import tailwind from "tailwindcss/vite";
 import { fileURLToPath } from "url";
 import { URL } from "url";
 
 export default defineConfig({
-  base: "/",
+  base: "./",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
